b22bd7a17800655fb085d98f799382056f8dcc0b
Nate Harris <nwithan8@users.noreply.github.com>
2026-06-03 22:39:19 -0600

    - Add channel QR code generation

--- static/data/dump_data.py
+++ static/data/dump_data.py
@@ -1,4 +1,5 @@
 import json
+import qrcode
 
 from colorado.airports import Airports
 from colorado.counties import Counties
@@ -6,9 +7,23 @@ from colorado.mountains import Mountains
 from colorado.municipalities import Municipalities
 from colorado.unincorporated_areas import UnincorporatedAreas
 from colorado.base.location import LocationEnum
+from coloradomesh.meshcore.services.channels import get_hashtag_channel_keys
+from coloradomesh.meshcore.utils import build_meshcore_channel_url
 
 DATA_FOLDER = "./static/data"
+CHANNEL_DETAILS_FILE = "channels.json"
+CHANNELS_QR_CODE_FOLDER = "./static/data/channels"
 
+
+class Channel:
+    name: str
+    order: int
+    description: str
+
+    def __init__(self, data: dict):
+        self.name = data["name"]
+        self.description = data["description"]
+        self.order = data["order"]
 
 def location_to_json(location: LocationEnum) -> dict:
     return json.loads(location._location.model_dump_json())
@@ -17,6 +32,22 @@ def location_to_json(location: LocationEnum) -> dict:
 def write_to_file(data: list[dict], filename):
     with open(f"{DATA_FOLDER}/{filename}", "w") as f:
         json.dump(data, f, indent=4)
+
+def read_from_file(filename: str):
+    with open(f"{DATA_FOLDER}/{filename}", "r") as f:
+        return json.load(f)
+
+
+def generate_channel_qr_code_image(channel: Channel):
+    name = channel.name
+    clean_name = name.replace("#", "")
+    order = channel.order
+
+    key, _ = get_hashtag_channel_keys(channel_name=name)
+    url = build_meshcore_channel_url(name=name, secret=key)
+
+    qr_code_image = qrcode.make(data=url)
+    qr_code_image.save(f"{CHANNELS_QR_CODE_FOLDER}/meshcore_channel_{order}_{clean_name}.png")
 
 
 airport_data = [location_to_json(location=airport) for airport in Airports]
@@ -33,3 +64,8 @@ write_to_file(data=municipalities_data, filename="municipalities.json")
 
 unincorporated_areas_data = [location_to_json(location=unincorporated_area) for unincorporated_area in UnincorporatedAreas]
 write_to_file(data=unincorporated_areas_data, filename="unincorporated_areas.json")
+
+channels_data = read_from_file(filename=CHANNEL_DETAILS_FILE)
+channels = [Channel(data=data) for data in channels_data]
+for _channel in channels:
+    generate_channel_qr_code_image(channel=_channel)
